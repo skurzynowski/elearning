6445ad6126457249655df5cab147c2a9bb82b9fb
skurzynowski <sebastian.kurzynowski@gmail.com>
2018-11-07 22:28:36 +0100

    fix admin

--- app/components/content/Post.jsx
+++ app/components/content/Post.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
   FormControl,
   Grid,
@@ -10,82 +10,88 @@ import {
   ControlLabel,
   Button,
   Panel
-} from "react-bootstrap";
-import fetchWp from "../../utils/fetchWP";
-import { connect } from "react-redux";
-import StartTestButton from "./StartTestButton";
+} from 'react-bootstrap'
+import fetchWp from '../../utils/fetchWP'
+import { connect } from 'react-redux'
+import StartTestButton from './StartTestButton'
 import {
   setActiveModule,
   setAppMode,
   setCurrentTest,
   setActiveSubmodule
-} from "../../../redux/appState/actions";
+} from '../../../redux/appState/actions'
 
 class Post extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       activePost: {},
       modules: this.props.modules,
       subModuleIndex: 0,
       moduleIndex: 0
-    };
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.activeSubmodule !== nextProps.activeSubmodule) {
+      this.setState({ moduleIndex: nextProps.activeSubmodule[0],subModuleIndex: nextProps.activeSubmodule[2]})
+    }
   }
 
   componentDidUpdate = prevProps => {
     if (this.props.activePost === prevProps.activePost) {
-      return;
+      return
     }
-  };
+  }
 
   onClickNextSubmoduleButton = () => {
     this.props.setActiveSubmodule(
-      this.state.moduleIndex + "_" + parseInt(this.state.subModuleIndex + 1)
-    );
-    this.setState({ subModuleIndex: this.state.subModuleIndex + 1 });
+      this.state.moduleIndex + '_' + parseInt(this.state.subModuleIndex + 1)
+    )
+    this.setState({subModuleIndex: this.state.subModuleIndex + 1})
     // this.forceUpdate()
-  };
+  }
 
   onClickNextModuleButton = () => {
-    let newModuleIndex = parseInt(this.state.moduleIndex + 1);
-    this.props.setActiveModule(newModuleIndex);
-    this.props.setActiveSubmodule(newModuleIndex + "_0");
-    this.setState({ subModuleIndex: 0, moduleIndex: newModuleIndex });
-  };
+    let newModuleIndex = parseInt(this.state.moduleIndex + 1)
+    this.props.setActiveModule(newModuleIndex)
+    this.props.setActiveSubmodule(newModuleIndex + '_0')
+    this.setState({subModuleIndex: 0, moduleIndex: newModuleIndex})
+  }
 
   onClickStartLastTest = () => {
-    this.props.setActiveSubmodule(null);
-    this.props.setActiveModule(null);
-    this.props.setCurrentTest("post-test");
-    this.props.setAppMode("test");
-  };
+    this.props.setActiveSubmodule(null)
+    this.props.setActiveModule(null)
+    this.props.setCurrentTest('post-test')
+    this.props.setAppMode('test')
+  }
 
   isLastSubmodule = () => {
     return (
       (typeof this.state.modules[this.state.moduleIndex].fields[
-        "module_content_" + (parseInt(this.state.subModuleIndex) + parseInt(1))
-      ] == "undefined" ||
+        'module_content_' + (parseInt(this.state.subModuleIndex) + parseInt(1))
+          ] == 'undefined' ||
         this.state.modules[this.state.moduleIndex].fields[
-          "module_content_" +
-            (parseInt(this.state.subModuleIndex) + parseInt(1))
-        ] == "") &&
+        'module_content_' +
+        (parseInt(this.state.subModuleIndex) + parseInt(1))
+          ] == '') &&
       (typeof this.state.modules[this.state.moduleIndex].fields[
-        "module_title_" + (parseInt(this.state.subModuleIndex) + parseInt(1))
-      ] == "undefined" ||
+        'module_title_' + (parseInt(this.state.subModuleIndex) + parseInt(1))
+          ] == 'undefined' ||
         this.state.modules[this.state.moduleIndex].fields[
-          "module_title_" + (parseInt(this.state.subModuleIndex) + parseInt(1))
-        ] == "")
-    );
-  };
+        'module_title_' + (parseInt(this.state.subModuleIndex) + parseInt(1))
+          ] == '')
+    )
+  }
 
   isLastModule = () => {
-    var module_index = this.state.moduleIndex + parseInt(1);
+    var module_index = this.state.moduleIndex + parseInt(1)
     return (
-      typeof this.state.modules[module_index] == "undefined" ||
-      (this.state.modules[module_index].fields["module_content_0"] == "" ||
-        this.state.modules[module_index].fields["module_title_0"] == "")
-    );
-  };
+      typeof this.state.modules[module_index] == 'undefined' ||
+      (this.state.modules[module_index].fields['module_content_0'] == '' ||
+        this.state.modules[module_index].fields['module_title_0'] == '')
+    )
+  }
 
   renderNextButton = () => {
     if (!this.isLastSubmodule()) {
@@ -93,52 +99,52 @@ class Post extends Component {
         <Button bsStyle="primary" onClick={this.onClickNextSubmoduleButton} bsSize="large">
           Dalej
         </Button>
-      );
+      )
     } else {
       if (this.isLastModule()) {
         return (
           <Button bsStyle="primary" onClick={this.onClickStartLastTest} bsSize="large">
             Test końcowy
           </Button>
-        );
+        )
       } else {
         return (
           <Button bsStyle="primary" onClick={this.onClickNextModuleButton} bsSize="large">
             Następny moduł
           </Button>
-        );
+        )
       }
     }
-  };
+  }
 
-  render() {
+  render () {
     return (
       <Col>
         <Grid componentClass="content-post">
           {typeof this.state.modules[this.state.moduleIndex].post_title !=
-          "undefined" ? (
+          'undefined' ? (
             <h3>{this.state.modules[this.state.moduleIndex].post_title}</h3>
           ) : null}
           <Panel>
             {typeof this.state.modules[this.state.moduleIndex].fields[
-              "module_title_" + this.state.subModuleIndex
-            ] != "undefined" ? (
+            'module_title_' + this.state.subModuleIndex
+              ] != 'undefined' ? (
               <Panel.Heading>
                 {
                   this.state.modules[this.state.moduleIndex].fields[
-                    "module_title_" + this.state.subModuleIndex
-                  ]
+                  'module_title_' + this.state.subModuleIndex
+                    ]
                 }
               </Panel.Heading>
             ) : null}
             {typeof this.state.modules[this.state.moduleIndex].fields[
-              "module_content_" + this.state.subModuleIndex
-            ] != "undefined" ? (
+            'module_content_' + this.state.subModuleIndex
+              ] != 'undefined' ? (
               <Panel.Body
                 dangerouslySetInnerHTML={{
                   __html: this.state.modules[this.state.moduleIndex].fields[
-                    "module_content_" + this.state.subModuleIndex
-                  ]
+                  'module_content_' + this.state.subModuleIndex
+                    ]
                 }}
               />
             ) : null}
@@ -146,7 +152,7 @@ class Post extends Component {
           </Panel>
         </Grid>
       </Col>
-    );
+    )
   }
 }
 
@@ -155,15 +161,16 @@ const mapDispatchToProps = dispatch => ({
   setCurrentTest: test => dispatch(setCurrentTest(test)),
   setActiveModule: module => dispatch(setActiveModule(module)),
   setActiveSubmodule: module => dispatch(setActiveSubmodule(module))
-});
+})
 
 const mapStateToProps = state => ({
   activePost: state.appState.activePost,
   fetchWP: state.appState.fetchWP,
-  modules: state.appState.modules
-});
+  modules: state.appState.modules,
+  activeSubmodule: state.appState.activeSubmodule,
+})
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Post);
+)(Post)
--- app/components/sitebar/SiteBarAdmin.jsx
+++ app/components/sitebar/SiteBarAdmin.jsx
@@ -1,151 +1,116 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Grid, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
-import { connect } from "react-redux";
-import { setActivePost, setModules } from "../../../redux/appState/actions";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { setActivePost, setActiveSubmodule,setActiveModule, setAppMode, setModules } from '../../../redux/appState/actions'
 
 class SiteBarAdmin extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
       adminList: [],
-      test: "123",
+      test: '123',
       endpointData: [],
       activeSubmodule: false
-    };
+    }
 
-    this.fetchModules();
+    this.fetchModules()
   }
 
   componentWillReceiveProps = nextProps => {
     if (nextProps.activeSubmodule != this.props.activeSubmodule) {
       // this.setState({adminList:[]})
       // this.setState({adminList:this.state.adminList})
-      this.setState({ activeSubmodule: nextProps.activeSubmodule });
+      this.setState({activeSubmodule: nextProps.activeSubmodule})
       // this.fillList(this.state.endpointData)
-      this.forceUpdate();
+      this.forceUpdate()
     }
-  };
+  }
 
   fetchModules = () => {
-    this.props.fetchWP.get("modules").then(json => {
-      this.setState({ adminList: json.modules });
-      this.props.setModules(json.modules);
-    });
-  };
+    this.props.fetchWP.get('modules').then(json => {
+      this.setState({adminList: json.modules})
+      this.props.setModules(json.modules)
+    })
+  }
 
   getActiveSubmodule = () => {
-    return this.props.activeSubmodule;
-  };
+    return this.props.activeSubmodule
+  }
 
   getActiveModule = () => {
-    return this.props.activeModule;
-  };
+    return this.props.activeModule
+  }
+  onClickModule = (key) => {
+    this.props.setActiveSubmodule(key)
+    this.props.setActiveModule(key[0])
+    this.props.setAppMode('post')
+  }
 
   fillList = modules => {
     return modules.map(
-      function(data, key) {
-        let tmp_array = [];
+      function (data, key) {
+        let tmp_array = []
         tmp_array.push(
           <ListGroupItem
             active={this.getActiveModule() == key}
+            key={key + '_module'}
             header={data.post_title}
           />
-        );
+        )
 
-        data.fields.module_title_0 !== undefined &&
-        data.fields.module_title_0 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.getActiveSubmodule() == key + "_0"}>
-                1. {data.fields.module_title_0}
-              </ListGroupItem>
-            )
-          : null;
-
-        data.fields.module_title_1 !== undefined &&
-        data.fields.module_title_1 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.getActiveSubmodule() == key + "_1"}>
-                2. {data.fields.module_title_1}
-              </ListGroupItem>
-            )
-          : null;
-
-        data.fields.module_title_2 !== undefined &&
-        data.fields.module_title_2 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.getActiveSubmodule() == key + "_2"}>
-                3. {data.fields.module_title_2}
-              </ListGroupItem>
-            )
-          : null;
-
-        data.fields.module_title_3 !== undefined &&
-        data.fields.module_title_3 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.getActiveSubmodule() == key + "_3"}>
-                4. {data.fields.module_title_3}
-              </ListGroupItem>
-            )
-          : null;
-
-        data.fields.module_title_4 !== undefined &&
-        data.fields.module_title_4 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.props.activeSubmodule == key + "_4"}>
-                5. {data.fields.module_title_4}
-              </ListGroupItem>
-            )
-          : null;
-
-        data.fields.module_title_5 !== undefined &&
-        data.fields.module_title_5 !== ""
-          ? tmp_array.push(
-              <ListGroupItem active={this.props.activeSubmodule == key + "_5"}>
-                6. {data.fields.module_title_5}
-              </ListGroupItem>
-            )
-          : null;
-
-        return tmp_array;
+        for (let i = 0; i < 6; i++) {
+          let elementName = 'module_title_' + i
+          let submodulNumber = key + '_' + i
+          !!data.fields[elementName] && tmp_array.push(<ListGroupItem
+            onClick={() => this.onClickModule(submodulNumber)}
+            active={this.getActiveSubmodule() == submodulNumber} key={elementName}>
+            1. {data.fields[elementName]}
+          </ListGroupItem>)
+        }
+        return tmp_array
       }.bind(this)
-    );
-  };
+    )
+  }
 
-  render() {
+  render () {
     return (
       <div className="sitebar-admin-wraper">
         <ListGroup className="sitebar-admin-course-list">
           <ListGroupItem
             header="Test wstępny"
-            active={this.props.currentTest == "pre-test"}
+            active={this.props.currentTest == 'pre-test'}
           />
           {this.fillList(this.state.adminList)}
           <ListGroupItem
             header="Test końcowy"
-            active={this.props.currentTest == "post-test"}
+            active={this.props.currentTest == 'post-test'}
           />
         </ListGroup>
       </div>
-    );
+    )
   }
 }
 
-const mapDispatchToProps = {
-  activePost: post => setActivePost(post),
-  setModules: modules => setModules(modules)
-};
+const mapDispatchToProps = dispatch => ({
+  activePost: post => dispatch(setActivePost(post)),
+  setModules: modules => dispatch(setModules(modules)),
+  setActiveSubmodule: submodule => dispatch(setActiveSubmodule(submodule)),
+  setActiveModule: module => dispatch(setActiveModule(module)),
+  setAppMode: mode => dispatch(setAppMode(mode)),
+})
 
 const mapStateToProps = state => ({
   listOfTests: state.appState.listOfTests,
   currentTest: state.appState.currentTest,
   fetchWP: state.appState.fetchWP,
   activeModule: state.appState.activeModule,
-  activeSubmodule: state.appState.activeSubmodule
-});
+  activeSubmodule: state.appState.activeSubmodule,
+
+})
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SiteBarAdmin);
+)(SiteBarAdmin)
